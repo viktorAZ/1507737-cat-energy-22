dc41463f0f4b587d89e39748609361ab773e8b47
zdorovcev viktor <v.zdvacew@gmail.com>
2021-04-11 13:01:07 +0300

    Исправлены ошибки

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,17 @@
 // Burger
-const navMain = document.querySelector('.main-nav');
-const navToggle = document.querySelector('.main-nav__toggle');
+(function () {
+  const navMain = document.querySelector('.main-nav');
+  const navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--no-js');
+  navMain.classList.remove('main-nav--no-js');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+})();
